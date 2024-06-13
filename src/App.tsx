dc973cf5ff77a60f7baa6baea3a2104b1036016c
xStranged <fede.valle04@gmail.com>
2024-06-13 02:13:42 -0300

    feat: :sparkles: create a event using supabase

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import reactLogo from './assets/react.svg';
 import { Button } from 'primereact/button';
 import { InputText } from 'primereact/inputtext';
@@ -9,20 +9,39 @@ import 'primeflex/primeflex.css'; // flex
 import './App.css';
 import { Calendar } from 'primereact/calendar';
 import { Nullable } from "primereact/ts-helpers";
-import { logDate } from '../helpers/logDate'
+import { logDate } from '../utils/logDate'
 import { Badge } from 'primereact/badge';
+import { supabase } from '../utils/supabase'
+import { Dialog } from 'primereact/dialog';
+import { Toast, ToastMessage } from 'primereact/toast';
 
 function App() {
   const [date, setDate] = useState<Nullable<Date>>(null);
   const [code, setCode] = useState(null);
   
+  const toast = useRef<Toast>(null);
+
   useEffect(() => {
+
+    // TEST
+    
+    // const getCountries = async () => {
+    //   let { data } = await supabase.from('countries').select('name')
+    //   console.log(data);
+    // }
+    // getCountries()
+
+
     const searchParams = new URLSearchParams(window.location.search);
     const code = searchParams.get('code');
     setCode(code)
   }, [])
 
-  
+    const showSuccess = () => {
+      toast.current?.show({severity:'success', summary: 'El evento', detail:'Ha sido creado con exito', life: 3000});
+    }
+
+
   const Disponibility = () => {
 
     const [disponibility, setDisponibility] = useState<Nullable<Date>>(null);
@@ -106,7 +125,7 @@ function App() {
 
       <section>
         <header>
-          <h2>Event Name</h2>
+          <h2>Nombre del evento</h2>
           <p>codigo: {code}</p>
         </header>
         <div className='section-main'>
@@ -135,16 +154,67 @@ function App() {
     
   }
 
+  const handleCreate = async (hide, e, code, eventName) => {
+    
+    const { data, error } = await supabase
+    .from('event')
+    .insert([
+      { code: code, event_name: eventName },
+    ])
+    .select()
+    
+    if (!error){
+      showSuccess()
+      setCode(code)
+      hide(e)
+    }
+    
+    
+  }
+
+  const CreateEvent = ( {hide} ) => {
+
+    const [eventName, setEventName] = useState<string>('');
+    const [code, setCode] = useState<string>('');
+    
+    return(
+      <div className="flex flex-column px-8 py-5 gap-4" style={{ borderRadius: '12px', backgroundImage: 'radial-gradient(circle at left top, var(--primary-400), var(--primary-700))' }}>
+          <div className="inline-flex flex-column gap-2">
+              <label htmlFor="eventname" className="text-primary-50 font-semibold">
+                  Nombre evento
+              </label>
+              <InputText id="eventname" label="eventname" value={eventName} onChange={(e) => setEventName((e.target.value))} ></InputText>
+          </div>
+          <div className="inline-flex flex-column gap-2">
+              <label htmlFor="code" className="text-primary-50 font-semibold">
+                  Código
+              </label>
+              <InputText id="code" label="code" value={code} onChange={(e) => setCode((e.target.value))} ></InputText>
+          </div>
+          <div className="flex align-items-center gap-2">
+              <Button label="Crear" severity='success' onClick={(e) => handleCreate(hide, e, code, eventName)} className="p-3 w-full"></Button>
+              <Button label="Cancelar" onClick={(e) => hide(e)} text className="p-3 w-full text-primary-50 border-1 border-white-alpha-30 hover:bg-white-alpha-10"></Button>
+          </div>
+      </div>
+  )
+  }
   const NoCodeSection = () => {
     const [inputCode, setInputCode] = useState<string>();
+    const [dialogVisibility, setDialogVisibility] = useState<boolean>(false);
 
     return(
       <div>
         <h2>Ingresa a una sala</h2>
           <InputText placeholder='Codigo' value={inputCode} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setInputCode(e.target.value)} />
           <a href="http://localhost:5173?code=fasfa" style={{marginLeft: 10}}><Button label='Ingresar' /></a>
-        <h2>Crear sala</h2>
-        <Button label='Crear sala' />
+        <h2>Crear evento</h2>
+        <Button label='Crear evento' onClick={() => setDialogVisibility(true)} />
+        <Dialog
+            visible={dialogVisibility}
+            modal
+            onHide={() => {if (!dialogVisibility) return; setDialogVisibility(false); }}
+            content={({ hide }) => (<CreateEvent hide={hide}/>)}
+        ></Dialog>
       </div>
     )
   }
@@ -154,6 +224,7 @@ function App() {
       
         <div className="card justify-content-center">
           <h1>Calendary</h1>
+          <Toast ref={toast} position="top-center" />
           {(!code) && (<NoCodeSection />)}
           {(code) && (<SectionCalendarys />)}
         </div>
